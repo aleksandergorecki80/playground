b5e6a88c8008c8d684f886201c51dff95eb2e820
FoxCote <aleksandergorecki80@gmail.com>
2020-01-03 17:30:36 +0000

    excercises

---
+++ objectTry.js
@@ -0,0 +1,15 @@
+const shipsList = [
+
+  ];
+  
+  
+  function Test(id){
+    this.id = id;
+  }
+  
+
+  for(let i=0; i<10; i++){
+   shipsList.push(name = new Test(i)) 
+  }
+  
+  console.log(shipsList);
